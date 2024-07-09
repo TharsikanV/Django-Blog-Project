e0f23892bf7618b99cedfdf0a3233018ee39deab
TharsikanV <108121067+TharsikanV@users.noreply.github.com>
2024-07-09 23:52:20 +0530

    Many to One relationship betweenPost and Category

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,21 @@ from django.db import models
 from django.utils.text import slugify
 
 # Create your models here.
+# Category
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Post
 class Post(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField()
     img_url=models.URLField(null=True)#nullable coloumn aaka maaththa
     created_at=models.DateTimeField(auto_now_add=True)
     slug=models.SlugField(unique=True)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)#category ah delete panna athu sampanthamaana post delete aakidum
 
     def save(self,*args,**kwargs):
         self.slug=slugify(self.title)
@@ -17,9 +26,3 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-# Category
-class Category(models.Model):
-    name=models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
