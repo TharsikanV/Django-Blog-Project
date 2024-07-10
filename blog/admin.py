97fe952f3f6773fccb6da449519a1a9e4dfed429
TharsikanV <108121067+TharsikanV@users.noreply.github.com>
2024-07-11 01:39:03 +0530

    Customize Admin Interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Post,Category
 
+# # Register your models here.
+# admin.site.register(Post)
+# admin.site.register(Category)
+
+# customize admin interface
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','content')#enna field kaadanumnu sollalaam
+    search_fields=('title','content')#search box ah uruvaakitharum antha fiels kku
+    list_filter=('category','created_at')
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
 admin.site.register(Category)
